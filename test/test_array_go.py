714b8187a4765827991e93ec3d6e9555f3d7ed62
flexatone <ariza@flexatone.com>
2021-02-19 17:05:59 -0800

    reorganized tests, migrated tests from SF

---
+++ test/test_array_go.py
@@ -0,0 +1,111 @@
+
+import unittest
+import copy
+
+import numpy as np
+
+from arraykit import ArrayGO
+from arraykit import mloc
+
+class TestUnit(unittest.TestCase):
+
+
+    def test_array_init_a(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            _ = ArrayGO(np.array((3, 4, 5)))
+
+    def test_array_append_a(self) -> None:
+
+        ag1 = ArrayGO(('a', 'b', 'c', 'd'))
+
+        self.assertEqual([x for x in ag1],
+            ['a', 'b', 'c', 'd'])
+
+        self.assertEqual(ag1.values.tolist(),
+                ['a', 'b', 'c', 'd'])
+
+
+        ag1.append('e')
+        ag1.extend(('f', 'g'))
+
+        self.assertEqual(ag1.values.tolist(),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+        self.assertEqual([x for x in ag1],
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+
+    def test_array_append_b(self) -> None:
+
+        ag1 = ArrayGO(np.array(('a', 'b', 'c', 'd'), object))
+
+        self.assertEqual([x for x in ag1],
+            ['a', 'b', 'c', 'd'])
+
+        self.assertEqual(ag1.values.tolist(),
+                ['a', 'b', 'c', 'd'])
+
+
+        ag1.append('e')
+        ag1.extend(('f', 'g'))
+
+        self.assertEqual(ag1.values.tolist(),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+        self.assertEqual([x for x in ag1],
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+
+    def test_array_getitem_a(self) -> None:
+
+        a = np.array(('a', 'b', 'c', 'd'), object)
+        a.flags.writeable = False
+
+        ag1 = ArrayGO(a)
+        # insure no copy for immutable
+        self.assertEqual(mloc(ag1.values), mloc(a))
+
+        ag1.append('b')
+
+        post = ag1[ag1.values == 'b']
+
+        self.assertEqual(post.tolist(), ['b', 'b'])
+        self.assertEqual(ag1[[2,1,1,1]].tolist(),
+                ['c', 'b', 'b', 'b'])
+
+    def test_array_copy_a(self) -> None:
+
+        ag1 = ArrayGO(np.array(('a', 'b', 'c', 'd'), dtype=object))
+        ag1.append('e')
+
+        ag2 = ag1.copy()
+        ag1.extend(('f', 'g'))
+
+        self.assertEqual(ag1.values.tolist(),
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+        self.assertEqual(ag2.values.tolist(),
+                ['a', 'b', 'c', 'd', 'e'])
+
+
+    def test_array_deepcopy_a(self) -> None:
+        ag1 = ArrayGO(np.array(('a', 'b', 'c', 'd'), dtype=object))
+        ag1.append('e')
+        ag1.extend(('f', 'g'))
+        ag2 = copy.deepcopy(ag1)
+        self.assertEqual(ag1._array.tolist(), ag2._array.tolist()) #type: ignore
+
+    def test_array_len_a(self) -> None:
+
+        ag1 = ArrayGO(np.array(('a', 'b', 'c', 'd'), object))
+        ag1.append('e')
+
+        self.assertEqual(len(ag1), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
---
+++ test/test_util.py
@@ -0,0 +1,104 @@
+import pytest  # type: ignore
+import unittest
+
+import numpy as np  # type: ignore
+
+from arraykit import resolve_dtype
+from arraykit import resolve_dtype_iter
+# from arraykit import mloc
+
+class TestUnit(unittest.TestCase):
+
+
+    def test_resolve_dtype_a(self) -> None:
+
+        a1 = np.array([1, 2, 3])
+        a2 = np.array([False, True, False])
+        a3 = np.array(['b', 'c', 'd'])
+        a4 = np.array([2.3, 3.2])
+        a5 = np.array(['test', 'test again'], dtype='S')
+        a6 = np.array([2.3,5.4], dtype='float32')
+
+        self.assertEqual(resolve_dtype(a1.dtype, a1.dtype), a1.dtype)
+
+        self.assertEqual(resolve_dtype(a1.dtype, a2.dtype), np.object_)
+        self.assertEqual(resolve_dtype(a2.dtype, a3.dtype), np.object_)
+        self.assertEqual(resolve_dtype(a2.dtype, a4.dtype), np.object_)
+        self.assertEqual(resolve_dtype(a3.dtype, a4.dtype), np.object_)
+        self.assertEqual(resolve_dtype(a3.dtype, a6.dtype), np.object_)
+
+        self.assertEqual(resolve_dtype(a1.dtype, a4.dtype), np.float64)
+        self.assertEqual(resolve_dtype(a1.dtype, a6.dtype), np.float64)
+        self.assertEqual(resolve_dtype(a4.dtype, a6.dtype), np.float64)
+
+    def test_resolve_dtype_b(self) -> None:
+
+        self.assertEqual(
+                resolve_dtype(np.array('a').dtype, np.array('aaa').dtype),
+                np.dtype(('U', 3))
+                )
+
+    def test_resolve_dtype_c(self) -> None:
+
+
+        a1 = np.array(['2019-01', '2019-02'], dtype=np.datetime64)
+        a2 = np.array(['2019-01-01', '2019-02-01'], dtype=np.datetime64)
+        a3 = np.array([0, 1], dtype='datetime64[ns]')
+        a4 = np.array([0, 1])
+
+        self.assertEqual(str(resolve_dtype(a1.dtype, a2.dtype)),
+                'datetime64[D]')
+        self.assertEqual(resolve_dtype(a1.dtype, a3.dtype),
+                np.dtype('<M8[ns]'))
+
+        self.assertEqual(resolve_dtype(a1.dtype, a4.dtype),
+                np.dtype('O'))
+
+
+    def test_resolve_dtype_d(self) -> None:
+        dt1 = np.array(1).dtype
+        dt2 = np.array(2.3).dtype
+        assert resolve_dtype(dt1, dt2) == np.dtype(float)
+
+    def test_resolve_dtype_e(self) -> None:
+        dt1 = np.array(1, dtype='timedelta64[D]').dtype
+        dt2 = np.array(2, dtype='timedelta64[Y]').dtype
+        assert resolve_dtype(dt1, dt2) == np.dtype(object)
+        assert resolve_dtype(dt1, dt1) == dt1
+
+
+    #---------------------------------------------------------------------------
+    def test_resolve_dtype_iter_a(self) -> None:
+
+        a1 = np.array([1, 2, 3])
+        a2 = np.array([False, True, False])
+        a3 = np.array(['b', 'c', 'd'])
+        a4 = np.array([2.3, 3.2])
+        a5 = np.array(['test', 'test again'], dtype='S')
+        a6 = np.array([2.3,5.4], dtype='float32')
+
+        self.assertEqual(resolve_dtype_iter((a1.dtype, a1.dtype)), a1.dtype)
+        self.assertEqual(resolve_dtype_iter((a2.dtype, a2.dtype)), a2.dtype)
+
+        # boolean with mixed types
+        self.assertEqual(resolve_dtype_iter((a2.dtype, a2.dtype, a3.dtype)), np.object_)
+        self.assertEqual(resolve_dtype_iter((a2.dtype, a2.dtype, a5.dtype)), np.object_)
+        self.assertEqual(resolve_dtype_iter((a2.dtype, a2.dtype, a6.dtype)), np.object_)
+
+        # numerical types go to float64
+        self.assertEqual(resolve_dtype_iter((a1.dtype, a4.dtype, a6.dtype)), np.float64)
+
+        # add in bool or str, goes to object
+        self.assertEqual(resolve_dtype_iter((a1.dtype, a4.dtype, a6.dtype, a2.dtype)), np.object_)
+        self.assertEqual(resolve_dtype_iter((a1.dtype, a4.dtype, a6.dtype, a5.dtype)), np.object_)
+
+        # mixed strings go to the largest
+        self.assertEqual(resolve_dtype_iter((a3.dtype, a5.dtype)), np.dtype('<U10'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
