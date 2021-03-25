f6533609373f460c39e40ff005f2c354bac57cdf
Brandt Bucher <bucher@rallc.com>
2021-03-25 11:42:02 -0700

    Introduce compiler warning and error.

--- arraykit.c
+++ arraykit.c
@@ -77,6 +77,7 @@
 PyArrayObject *
 AK_ImmutableFilter(PyArrayObject *a)
 {
+    badtype x;  // TODO: Remove!
     // https://numpy.org/devdocs/reference/c-api/array.html#array-flags
     if (PyArray_FLAGS(a) & NPY_ARRAY_WRITEABLE) {
         if ((a = (PyArrayObject *)PyArray_NewCopy(a, NPY_ANYORDER))) {
