981931bb0f61eea531733a2ad9503faa9e54cf8f
flexatone <ariza@flexatone.com>
2021-04-11 06:29:27 -0700

    progress on type evaluation

--- test/test_type_discovery.py
+++ test/test_type_discovery.py
@@ -57,6 +57,12 @@ def is_space(c: str) -> bool:
 def is_sign(c: str) -> bool:
     return c == '+' or c == '-'
 
+def is_paren_open(c: str) -> bool:
+    return c == '('
+
+def is_paren_close(c: str) -> bool:
+    return c == ')'
+
 def is_decimal(c: str) -> bool:
     return c == '.'
 
@@ -67,32 +73,36 @@ def is_decimal(c: str) -> bool:
 #     assert ord(match) <= 90 # must be upper case
 #     return c == match or c == chr(ord(match) | 0x20)
 
+def is_a(c: str) -> bool:
+    return c == 'a' or c == 'A'
+
 def is_e(c: str) -> bool:
     return c == 'e' or c == 'E'
-
-def is_j(c: str) -> bool:
-    return c == 'j' or c == 'J'
-
-def is_t(c: str) -> bool:
-    return c == 't' or c == 'T'
-
-def is_r(c: str) -> bool:
-    return c == 'r' or c == 'R'
-
-def is_u(c: str) -> bool:
-    return c == 'u' or c == 'U'
 
 def is_f(c: str) -> bool:
     return c == 'f' or c == 'F'
 
-def is_a(c: str) -> bool:
-    return c == 'a' or c == 'A'
+def is_j(c: str) -> bool:
+    return c == 'j' or c == 'J'
 
 def is_l(c: str) -> bool:
     return c == 'l' or c == 'L'
 
+def is_n(c: str) -> bool:
+    return c == 'n' or c == 'N'
+
+def is_r(c: str) -> bool:
+    return c == 'r' or c == 'R'
+
 def is_s(c: str) -> bool:
     return c == 's' or c == 'S'
+
+def is_t(c: str) -> bool:
+    return c == 't' or c == 'T'
+
+def is_u(c: str) -> bool:
+    return c == 'u' or c == 'U'
+
 
 class TypeResolved(Enum):
     IS_UNKNOWN = 1
@@ -121,14 +131,18 @@ class TypeField:
         self.count_leading_space = 0
         self.count_bool = 0 # signed, not greater than +/- 5
 
-        # numeric symbols; values do not need to be greater than 2
+        # numeric symbols; values do not need to be greater than 4
         self.count_sign = 0
         self.count_e = 0
         self.count_j = 0
         self.count_decimal = 0
+        self.count_nan = 0
+        self.count_paren_open = 0
+        self.count_paren_close = 0
 
         # can be unbound in size
         self.count_digit = 0
+        self.count_notspace = 0 # non-digit, non-space
 
     def process_char(self, c: str, pos: int) -> int:
         # position is postion needs to be  dropping leading space
@@ -144,22 +158,29 @@ class TypeField:
         if is_space(c):
             if pos == 0:
                 self.previous_leading_space = True
-                self.count_leading_space += 1
-                return 1
+
             if self.previous_leading_space:
                 self.count_leading_space += 1
                 return 1
             space = True
+        else:
+            self.count_notspace += 1
+
 
         self.previous_leading_space = False # this char is not space
 
         pos_field = pos - self.count_leading_space
 
-        # evaluate numeric -----------------------------------------------------
+        # evaluate numeric, non-positional -------------------------------------
         numeric = False
+        digit = False
 
-        if is_digit(c):
+        if space:
+            pass
+
+        elif is_digit(c):
             numeric = True
+            digit = True
             self.count_digit += 1
 
         elif is_sign(c):
@@ -170,11 +191,27 @@ class TypeField:
                 return 0
             numeric = True
 
+        elif is_paren_open(c):
+            numeric = True
+            self.count_paren_open += 1
+            # open paren only permitted at pos_field 0
+            if pos_field != 0 or self.count_paren_open > 1:
+                self.resolved_field = TypeResolved.IS_STRING
+                return 0
+
+        elif is_paren_close(c):
+            numeric = True
+            self.count_paren_close += 1
+            # NOTE: not evaluating that this is on last position of contiguous numeric
+            if self.count_paren_close > 1:
+                self.resolved_field = TypeResolved.IS_STRING
+                return 0
+
         elif is_e(c): # only character that is numeric and bool
             numeric = True
             self.count_e += 1
-            if pos_field == 0 or self.count_e > 1:
-                # true and false each only have one E
+            if pos_field == 0 or self.count_e > 2:
+                # true and false each only have one E, complex can have 2
                 self.resolved_field = TypeResolved.IS_STRING
                 return 0
 
@@ -193,63 +230,71 @@ class TypeField:
                 return 0
 
         #-----------------------------------------------------------------------
-        print(f' pre: {c=} {pos=} {pos_field=} {numeric=} {self.previous_numeric=} {self.contiguous_numeric=}')
+        # print(f' pre: {c=} {pos=} {pos_field=} {numeric=} {self.previous_numeric=} {self.contiguous_numeric=}')
+
         if numeric:
             if pos_field == 0:
                 self.contiguous_numeric = True
                 self.previous_numeric = True
                 return 1 # E can not be in first position
+
             # pos_field > 0
             if not self.previous_numeric:
                 # found a numeric not in pos 0 where previous was not numeric
                 self.contiguous_numeric = False
 
             self.previous_numeric = True
+
             # NOTE: we need to consider possible Boolean scenario
             if self.contiguous_numeric or not is_e(c):
                 return 1
-        else:
+        else: # not numeric, could be space or notspace
             if self.contiguous_numeric and not space:
                 # if we find a non-numeric, non-space, after contiguous numeric
                 self.resolved_field = TypeResolved.IS_STRING
                 return 0
             self.previous_numeric = False
 
-        print(f'post: {c=} {pos=} {pos_field=} {numeric=} {self.previous_numeric=} {self.contiguous_numeric=}')
 
         # evaluate character positions -----------------------------------------
+        if space or digit:
+            return 1
+
+        # print(f'post: {c=} {pos=} {pos_field=} {numeric=} {self.previous_numeric=} {self.contiguous_numeric=} {self.count_notspace=}')
 
         if pos_field == 0:
             if is_t(c):
                 self.count_bool += 1
-            if is_f(c):
+            elif is_f(c):
                 self.count_bool -= 1
+            elif is_n(c):
+                self.count_nan += 1
 
         elif pos_field == 1:
             if is_r(c):
                 self.count_bool += 1
-            if is_a(c):
+            elif is_a(c):
                 self.count_bool -= 1
+                self.count_nan += 1
 
         elif pos_field == 2:
             if is_u(c):
                 self.count_bool += 1
-            if is_l(c):
+            elif is_l(c):
                 self.count_bool -= 1
+            elif is_n(c):
+                self.count_nan += 1
 
         elif pos_field == 3:
-            if is_e(c) and self.count_bool == 3:
-                self.resolved_field = TypeResolved.IS_BOOL
-                return 0
+            if is_e(c):
+                self.count_bool += 1
             if is_s(c):
                 self.count_bool -= 1
-
         elif pos_field == 4:
             if is_e(c) and self.count_bool == -4:
-                self.resolved_field = TypeResolved.IS_BOOL
-                return 0
+                self.count_bool -= 1
 
-        return 1 # keep going
+        return 1
 
     def resolve_field_type(self, count: int) -> None:
         '''
@@ -257,20 +302,45 @@ class TypeField:
         '''
         if count == 0:
             return TypeResolved.IS_EMPTY
+
         if self.resolved_field != TypeResolved.IS_UNKNOWN:
             return self.resolved_field
+        if self.count_bool == 4 and self.count_notspace == 4:
+            return TypeResolved.IS_BOOL
+        if self.count_bool == -5 and self.count_notspace == 5:
+            return TypeResolved.IS_BOOL
+        if self.count_nan == 3 and self.count_notspace == 3:
+            return TypeResolved.IS_FLOAT
 
         # determine
         if self.contiguous_numeric:
             # NOTE: have already handled cases with excessive counts
             if self.count_digit == 0:
-                # can have contiguous numerics like +ej.- but not digits
+                # can have contiguous numerics like +ej.- but no digits
                 return TypeResolved.IS_STRING
-            if self.count_j == 0 and self.count_e == 0 and self.count_decimal == 0:
+
+            if (self.count_j == 0
+                    and self.count_e == 0
+                    and self.count_decimal == 0
+                    and self.count_paren_open == 0
+                    and self.count_paren_close == 0):
                 return TypeResolved.IS_INT
-            if self.count_j == 0 and (self.count_decimal == 1 or self.count_e > 0):
+
+            if (self.count_j == 0
+                    and self.count_paren_open == 0
+                    and self.count_paren_close == 0
+                    and (self.count_decimal == 1 or self.count_e == 1)):
                 return TypeResolved.IS_FLOAT
-            if self.count_j == 1:
+
+            if self.count_j == 1 and (
+                    (self.count_paren_open == 1 and self.count_paren_close == 1)
+                    or (self.count_paren_open == 0 and self.count_paren_close == 0)
+                    ):
+                return TypeResolved.IS_COMPLEX
+            # if only paren and digits, mark as complex
+            if self.count_j == 0 and (
+                    (self.count_paren_open == 1 and self.count_paren_close == 1)
+                    ):
                 return TypeResolved.IS_COMPLEX
 
         return TypeResolved.IS_STRING
@@ -317,7 +387,7 @@ class TypeField:
         raise NotImplementedError(previous, new)
 
     def process(self, field: str) -> TypeResolved:
-        print(f'process: {field=}')
+        # print(f'process: {field=}')
 
         self.reset() # does not reset resolved_line
         pos = 0
@@ -330,7 +400,7 @@ class TypeField:
         # must call after all chars processed, does not set self.resolved field
         rlt_new = self.resolve_field_type(pos)
         self.resolved_line = self.resolve_line_type(self.resolved_line, rlt_new)
-        print(f'{self.resolved_line=}')
+        # print(f'{self.resolved_line=}')
         return self.resolved_line
 
     def process_line(self, fields: tp.Iterable[str]) -> TypeResolved:
@@ -338,12 +408,21 @@ class TypeField:
             self.process(field)
         return self.resolved_line
 
+
 class TestUnit(unittest.TestCase):
 
     def test_bool_a(self) -> None:
         self.assertEqual(TypeField().process('   true'), TypeResolved.IS_BOOL)
         self.assertEqual(TypeField().process('FALSE'), TypeResolved.IS_BOOL)
+        self.assertEqual(TypeField().process('FaLSE   '), TypeResolved.IS_BOOL)
+
         self.assertEqual(TypeField().process('  tals  '), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process('FALSEblah'), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process('   true f'), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process('   true3'), TypeResolved.IS_STRING)
+
+    def test_bool_b(self) -> None:
+        self.assertEqual(TypeField().process('   true +'), TypeResolved.IS_STRING)
 
 
     def test_str_a(self) -> None:
@@ -378,6 +457,8 @@ class TestUnit(unittest.TestCase):
 
     def test_float_b(self) -> None:
         self.assertEqual(TypeField().process(' 4e3'), TypeResolved.IS_FLOAT)
+        self.assertEqual(TypeField().process('4E3 '), TypeResolved.IS_FLOAT)
+
         self.assertEqual(TypeField().process(' 4e3e'), TypeResolved.IS_STRING)
         self.assertEqual(TypeField().process('4e3   e'), TypeResolved.IS_STRING)
         self.assertEqual(TypeField().process('e99   '), TypeResolved.IS_STRING)
@@ -387,6 +468,14 @@ class TestUnit(unittest.TestCase):
         self.assertEqual(TypeField().process('..'), TypeResolved.IS_STRING)
         self.assertEqual(TypeField().process('e+j.'), TypeResolved.IS_STRING)
 
+    def test_float_d(self) -> None:
+        self.assertEqual(TypeField().process('  nan'), TypeResolved.IS_FLOAT)
+        self.assertEqual(TypeField().process('NaN   '), TypeResolved.IS_FLOAT)
+
+        self.assertEqual(TypeField().process('NaN3   '), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process(' N an   '), TypeResolved.IS_STRING)
+
+
     def test_float_known_false_positive(self) -> None:
         # NOTE: we mark this as float because we do not observe that a number must follow e; assume this will fail in float conversion
         self.assertEqual(TypeField().process('8e'), TypeResolved.IS_FLOAT)
@@ -394,6 +483,7 @@ class TestUnit(unittest.TestCase):
     def test_complex_a(self) -> None:
         self.assertEqual(TypeField().process('23j  '), TypeResolved.IS_COMPLEX)
         self.assertEqual(TypeField().process(' 4e3j'), TypeResolved.IS_COMPLEX)
+
         self.assertEqual(TypeField().process(' 4e3jw'), TypeResolved.IS_STRING)
         self.assertEqual(TypeField().process(' J4e3j'), TypeResolved.IS_STRING)
         self.assertEqual(TypeField().process('-4.3+3j'), TypeResolved.IS_COMPLEX)
@@ -408,20 +498,45 @@ class TestUnit(unittest.TestCase):
 
         self.assertEqual(TypeField().process('+23-3.5j  +'), TypeResolved.IS_STRING)
 
-    def test_float_known_false_positive(self) -> None:
+    def test_complex_c(self) -> None:
+        self.assertEqual(TypeField().process(' (23+3j) '), TypeResolved.IS_COMPLEX)
+        self.assertEqual(TypeField().process('(4e3-4.5j)'), TypeResolved.IS_COMPLEX)
+        self.assertEqual(TypeField().process('(4.3)'), TypeResolved.IS_COMPLEX)
+
+        self.assertEqual(TypeField().process(' (23+3j)) '), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process(' (((23+3j'), TypeResolved.IS_STRING)
+        self.assertEqual(TypeField().process(' 2(3+3j) '), TypeResolved.IS_STRING)
+
+
+    def test_complex_known_false_positive(self) -> None:
         # NOTE: genfromtxt identifies this as string as j component is in first position
         self.assertEqual(TypeField().process('23j-43'), TypeResolved.IS_COMPLEX)
         self.assertEqual(TypeField().process('+23-3.5j3'), TypeResolved.IS_COMPLEX)
+        self.assertEqual(TypeField().process('(23+)3j '), TypeResolved.IS_COMPLEX)
 
 
     def test_line_a(self) -> None:
         self.assertEqual(TypeField().process_line(('25', '2.5', '')), TypeResolved.IS_FLOAT)
         self.assertEqual(TypeField().process_line((' .1', '2.5', '')), TypeResolved.IS_FLOAT)
         self.assertEqual(TypeField().process_line(('25', '', '')), TypeResolved.IS_INT)
+
         self.assertEqual(TypeField().process_line(('25', '2.5', 'e')), TypeResolved.IS_STRING)
 
-    def test_line_a(self) -> None:
+    def test_line_b(self) -> None:
         self.assertEqual(TypeField().process_line(('  true', '  false', 'FALSE')), TypeResolved.IS_BOOL)
+        self.assertEqual(TypeField().process_line(('  true', '  false', 'FALSEq')), TypeResolved.IS_STRING)
+
+    def test_line_c(self) -> None:
+        self.assertEqual(TypeField().process_line(('3', '', '4')), TypeResolved.IS_INT)
+
+        self.assertEqual(TypeField().process_line(('3', '', '4e')), TypeResolved.IS_FLOAT)
+        self.assertEqual(TypeField().process_line(('3', '', '.')), TypeResolved.IS_STRING)
+
+    def test_line_d(self) -> None:
+        self.assertEqual(TypeField().process_line(('3', '', '4.')), TypeResolved.IS_FLOAT)
+        self.assertEqual(TypeField().process_line(('3', '', '4e3')), TypeResolved.IS_FLOAT)
+        self.assertEqual(TypeField().process_line(('3', '', '(4e3)')), TypeResolved.IS_COMPLEX)
+
 
 if __name__ == '__main__':
     unittest.main()
