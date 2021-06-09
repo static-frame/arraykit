91c4aeef3a99e4aac5692ee5adc209156b05dc61
Charles Burkland <burkland@rallc.com>
2021-06-09 12:42:13 -0700

    Makes implicit type coercion explicit.

--- src/_arraykit.c
+++ src/_arraykit.c
@@ -397,7 +397,7 @@ dtype_from_element(PyObject *Py_UNUSED(m), PyObject *arg)
         }
 
         npy_uint64 uv = PyLong_AsUnsignedLongLong(arg);
-        if (uv == -1 && PyErr_Occurred()) {
+        if (uv == (npy_uint64)-1 && PyErr_Occurred()) {
             if (!PyErr_ExceptionMatches(PyExc_OverflowError)) {
                 return NULL;
             }
