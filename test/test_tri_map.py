0ea3be09eb409bd7ccc991c674c5c6b3dfb2d267
Christopher Ariza <ariza@flexatone.com>
2024-05-20 16:53:18 -0700

    intial interface of register_many

--- test/test_tri_map.py
+++ test/test_tri_map.py
@@ -75,3 +75,5 @@ class TestUnit(unittest.TestCase):
     def test_tri_map_register_many_a(self) -> None:
         tm = TriMap(100, 50)
         tm.register_many(3, np.array([2,5,8]))
+
+        tm.register_many("foo", np.array([2,5,8]))
