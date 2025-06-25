a9d92ef6af3312388e3a2db6252bdd4ce8fe9d20
Christopher Ariza <ariza@flexatone.com>
2025-06-24 18:01:05 -0700

    additional test

--- test/test_astype_array.py
+++ test/test_astype_array.py
@@ -14,3 +14,20 @@ class TestUnit(unittest.TestCase):
         self.assertEqual(id(a1), id(a2))
 
 
+    def test_astype_array_a2(self) -> None:
+        a1 = np.array([10, 20, 30], dtype=np.int64)
+        a1.flags.writeable = False
+
+        a2 = astype_array(a1, np.float64)
+        self.assertNotEqual(id(a1), id(a2))
+        self.assertEqual(a2.dtype, np.dtype(np.float64))
+
+
+
+    def test_astype_array_a3(self) -> None:
+        a1 = np.array([False, True, False])
+
+        a2 = astype_array(a1, np.int8)
+        self.assertEqual(a2.dtype, np.dtype(np.int8))
+        self.assertFalse(a2.flags.writeable)
+
