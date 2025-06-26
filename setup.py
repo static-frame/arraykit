d5aece1a6027bed38a67bb0f7345d40956a7131a
flexatone <ariza@flexatone.com>
2025-06-26 08:22:09 -0700

    added classifier

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Free Threading',
         'Typing :: Typed',
     ],
     keywords='numpy array',
